b3b3c8638768acdc7e91f2d464f7dfd9e3b05839
Marcos André <marcosandre@cc.ci.ufpb.br>
2022-07-27 15:01:43 -0300

    Parametrizar pageSize no MatchesService.getMatches

--- app/src/main/java/com/example/csgomatches/data/matches/service/MatchesRemoteDataSource.kt
+++ app/src/main/java/com/example/csgomatches/data/matches/service/MatchesRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.csgomatches.data.matches.service
 
 import com.example.csgomatches.data.API_TOKEN
+import com.example.csgomatches.data.PAGE_SIZE
 import com.example.csgomatches.data.retrofit
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -18,6 +19,7 @@ private interface MatchesService {
     @GET("csgo/matches")
     suspend fun getMatches(
         @Query("page[number]") page: Int,
+        @Query("page[size]") pageSize: Int = PAGE_SIZE,
         @Query("sort") sort: String = "-status,begin_at",
         @Query("filter[status]") statusFilter: String = "running,not_started",
         @Query("filter[unscheduled]") showUnscheduled: Boolean = false,
